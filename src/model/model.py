4d4468916549ab078f49a71b0cecd75059e1b292
Conor Sexton <sexton.conor@gmail.com>
2020-02-06 23:01:13 +0000

    Added coord encoding and pooling layers.  WIP: RNN cell

--- src/model/model.py
+++ src/model/model.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
-
+import logging
 import tensorflow as tf
 
 from tensorflow.keras.layers import Dense, Flatten, Conv2D
@@ -9,17 +9,54 @@ from rnn import ChaRNN
 
 
 class ReaderModel(tf.keras.Model):
-    def __init__(self, rnn_size):
+    def __init__(self, input_shape, seq_length, rnn_size, num_char_classes):
         super(ReaderModel, self).__init__()
-        self.feature_enc = tf.keras.applications.InceptionResNetV2(weights='imagenet', input_shape=(75,75,3), include_top=False)
-        self.feature_enc.outputs = [self.feature_enc.get_layer('mixed_6a').output]
-        self.rnn = ChaRNN(rnn_size)
+        self.feature_enc = tf.keras.applications.InceptionResNetV2(
+            weights='imagenet', input_shape=input_shape, include_top=False)
+        self.feature_enc.outputs = [
+            self.feature_enc.get_layer('mixed_6a').output]
+        self.rnn = ChaRNN(rnn_size, seq_length, num_char_classes)
 
+    @tf.function
     def call(self, x):
-        x = self.feature_enc(x)
-        # TODO need alpha of attention layer
-        x, _ = self.rnn(x)
-        return x
+        print(x.shape)
+        f = self.feature_enc(x)
+        print(f.shape)
+        f_enc = self.encode_coords(f)
+        print(f_enc.shape)
+        f_pool = self.pool_views(f_enc)
+        print(f_pool.shape)
+        logits = self.rnn((f_pool, None))
+        return logits
 
+    @tf.function
+    def encode_coords(self, f):
+        """Adds one-hot encoding of coordinates to different views in the networks.
+        For each "pixel" of a feature map it adds a onehot encoded x and y
+        coordinates.
+        Args:
+        net: a tensor of shape=[batch_size, height, width, num_features]
+        Returns:
+        a tensor with the same height and width, but altered feature_size.
+        """
+        batch_size, h, w, _ = f.shape
+        x, y = tf.meshgrid(tf.range(w), tf.range(h))
+        w_loc = tf.one_hot(x, depth=w)
+        h_loc = tf.one_hot(y, depth=h)
+        loc = tf.concat([h_loc, w_loc], 2)
+        loc = tf.tile(tf.expand_dims(loc, 0), [batch_size, 1, 1, 1])
+        return tf.concat([f, loc], 3)
 
-    
+    @tf.function
+    def pool_views(self, f_enc):
+        """Combines output of multiple convolutional towers into a single tensor.
+        It stacks towers one on top another (in height dim) in a 4x1 grid.
+        The order is arbitrary design choice and shouldn't matter much.
+        Args:
+        nets: list of tensors of shape=[batch_size, height, width, num_features].
+        Returns:
+        A tensor of shape [batch_size, seq_length, features_size].
+        """
+        batch_size = f_enc.shape[0]
+        feature_size = f_enc.shape[3]
+        return tf.reshape(f_enc, [batch_size, -1, feature_size])
--- src/model/rnn.py
+++ src/model/rnn.py
@@ -1,24 +1,97 @@
 from tensorflow.keras import layers
 import tensorflow as tf
+import sys
 
 class ChaRNN(layers.Layer):
+    '''
+    This class implements a custom layer with an LSTMCell to allow autocorelated links 
+    from each steps output to input
+    '''
 
-    def __init__(self, rnn_size):
+    def __init__(self, rnn_size, seq_length, num_char_classes):
         super(ChaRNN, self).__init__()
         self.rnn_size = rnn_size
-        self.cell = layers.LSTMCell(rnn_size)
-
+        self.cell = layers.LSTMCell((num_char_classes, rnn_size))
+        self.seq_length = seq_length
+        self._char_logits = {}
+        self.num_char_classes = num_char_classes
+        w_init = tf.random_normal_initializer()
+        self._softmax_w = tf.Variable(initial_value=w_init(shape=(rnn_size, num_char_classes),
+                                              dtype='float32'),
+                                      trainable=True)
+        self._softmax_b = tf.Variable(initial_value=w_init(shape=(rnn_size, num_char_classes),
+                                              dtype='float32'),
+                                      trainable=True)
 
     @tf.function
-    def call(self, input_data):
-        outputs = tf.TensorArray(tf.float32, size=0, dynamic_size=True)
-        state = tf.zeros((input_data.shape[0], self.rnn_size), dtype=tf.float32)
+    def call(self, inputs):
+        print('RNN')
+        f_pool, ground_truth = inputs
+        batch_size, timestep, _ = f_pool.shape
+        # Variable for collecting all outputs
+        outputs = []
+        
+        # Initial state and previous output
+        state = tf.zeros((batch_size, self.rnn_size), dtype=tf.float32)
+        prev_output = tf.zeros([batch_size, self.num_char_classes])
 
-        for i in tf.range(input_data.shape[1]):
-            print(input)
-            output, state = self.cell(tf.expand_dims(input_data[:, i, :], 1), state)
+        # for i in range(timestep):
+        for i in tf.range(1):
+            print(prev_output.shape)
+            output, state = self.cell(prev_output, state)
             print(output.shape)
-            outputs = outputs.write(i, output)
+            outputs = outputs.append(output)
+            if ground_truth is not None:
+                prev_output = ground_truth[:, i, :]
+            else:
+                prev_output = self.char_logit(output, i)
+                prev_output = self.char_one_hot(prev_output)
+
+
         return tf.transpose(outputs.stack(), [1, 0, 2]), state
+
+    @tf.function
+    def get_image_feature(self, f_enc, char_index):
+        """Returns a subset of image features for a character.
+        Args:
+        char_index: an index of a character.
+        Returns:
+        A tensor with shape [batch_size, ?]. The output depth depends on the
+        depth of input net.
+        """
+        batch_size, features_num, _ = f_enc.shape
+        slice_len = int(features_num / self.seq_length)
+        # In case when features_num != seq_length, we just pick a subset of image
+        # features, this choice is arbitrary and there is no intuitive geometrical
+        # interpretation. If features_num is not dividable by seq_length there will
+        # be unused image features.
+        f_slice = f_enc[:, char_index:char_index + slice_len, :]
+        feature = tf.reshape(f_slice, [batch_size, -1])
+        return feature
+
+    @tf.function
+    def char_logit(self, inputs, char_index):
+        """Creates logits for a character if required.
+        Args:
+        inputs: A tensor with shape [batch_size, ?] (depth is implementation
+            dependent).
+        char_index: A integer index of a character in the output sequence.
+        Returns:
+        A tensor with shape [batch_size, num_char_classes]
+        """
+        if char_index not in self._char_logits:
+            self._char_logits[char_index] = inputs * self._softmax_w + self._softmax_b
+        return self._char_logits[char_index]
+
+    @tf.function
+    def char_one_hot(self, logit):
+        """Creates one hot encoding for a logit of a character.
+        Args:
+        logit: A tensor with shape [batch_size, num_char_classes].
+        Returns:
+        A tensor with shape [batch_size, num_char_classes]
+        """
+        prediction = tf.argmax(logit, axis=1)
+        return slim.one_hot_encoding(prediction, self._params.num_char_classes)
 
 
