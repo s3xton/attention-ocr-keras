e38dc1c4b98c19fc387293a22a35e0b031671838
Conor Sexton <sexton.conor@gmail.com>
2020-02-12 22:14:12 +0000

    RNN working with autoregression

--- src/model/model.py
+++ src/model/model.py
@@ -9,7 +9,7 @@ from rnn import ChaRNN
 
 
 class ReaderModel(tf.keras.Model):
-    def __init__(self, input_shape, seq_length, rnn_size, num_char_classes):
+    def __init__(self, input_shape, seq_length, rnn_size, num_char_classes, is_training=True):
         super(ReaderModel, self).__init__()
         self.feature_enc = tf.keras.applications.InceptionResNetV2(
             weights='imagenet', input_shape=input_shape, include_top=False)
@@ -19,14 +19,16 @@ class ReaderModel(tf.keras.Model):
 
     @tf.function
     def call(self, x):
-        print(x.shape)
+        print('Input shape: {}'.format(x.shape))
         f = self.feature_enc(x)
-        print(f.shape)
+        print('Feature enc shape: {}'.format(f.shape))
         f_enc = self.encode_coords(f)
-        print(f_enc.shape)
+        print('Feature enc with OH coords shape: {}'.format(f_enc.shape))
         f_pool = self.pool_views(f_enc)
-        print(f_pool.shape)
-        logits = self.rnn((f_pool, None))
+        print('Pooled feature enc shape: {}'.format(f_pool.shape))
+        logits, state = self.rnn((f_pool, None))
+        print('Char logits shape: {}'.format(logits.shape))
+        # predicted_chars, chars_log_prob, predicted_scores = (self.char_predictions(chars_logit))
         return logits
 
     @tf.function
@@ -60,3 +62,28 @@ class ReaderModel(tf.keras.Model):
         batch_size = f_enc.shape[0]
         feature_size = f_enc.shape[3]
         return tf.reshape(f_enc, [batch_size, -1, feature_size])
+
+    @tf.function
+    def char_predictions(self, chars_logit):
+        """Returns confidence scores (softmax values) for predicted characters.
+        Args:
+        chars_logit: chars logits, a tensor with shape
+            [batch_size x seq_length x num_char_classes]
+        Returns:
+        A tuple (ids, log_prob, scores), where:
+            ids - predicted characters, a int32 tensor with shape
+            [batch_size x seq_length];
+            log_prob - a log probability of all characters, a float tensor with
+            shape [batch_size, seq_length, num_char_classes];
+            scores - corresponding confidence scores for characters, a float
+            tensor
+            with shape [batch_size x seq_length].
+        """
+        log_prob = utils.logits_to_log_prob(chars_logit)
+        ids = tf.to_int32(tf.argmax(log_prob, axis=2), name='predicted_chars')
+        mask = tf.cast(
+        slim.one_hot_encoding(ids, self._params.num_char_classes), tf.bool)
+        all_scores = tf.nn.softmax(chars_logit)
+        selected_scores = tf.boolean_mask(all_scores, mask, name='char_scores')
+        scores = tf.reshape(selected_scores, shape=(-1, self._params.seq_length))
+        return ids, log_prob, scores
--- src/model/rnn.py
+++ src/model/rnn.py
@@ -11,47 +11,57 @@ class ChaRNN(layers.Layer):
     def __init__(self, rnn_size, seq_length, num_char_classes):
         super(ChaRNN, self).__init__()
         self.rnn_size = rnn_size
-        self.cell = layers.LSTMCell((num_char_classes, rnn_size))
-        self.seq_length = seq_length
-        self._char_logits = {}
         self.num_char_classes = num_char_classes
+        self.seq_length = tf.constant(seq_length)
+        self.cell = layers.LSTMCell(self.rnn_size)
+        self.dense = layers.Dense(self.num_char_classes)
+        self._char_logits = {}
+        
         w_init = tf.random_normal_initializer()
-        self._softmax_w = tf.Variable(initial_value=w_init(shape=(rnn_size, num_char_classes),
+        self._softmax_w = tf.Variable(initial_value=w_init(shape=(self.rnn_size, self.num_char_classes),
                                               dtype='float32'),
                                       trainable=True)
-        self._softmax_b = tf.Variable(initial_value=w_init(shape=(rnn_size, num_char_classes),
+        self._softmax_b = tf.Variable(initial_value=w_init(shape=(self.num_char_classes,),
                                               dtype='float32'),
                                       trainable=True)
 
     @tf.function
     def call(self, inputs):
-        print('RNN')
         f_pool, ground_truth = inputs
-        batch_size, timestep, _ = f_pool.shape
-        # Variable for collecting all outputs
-        outputs = []
+        batch_size, _, _ = f_pool.shape
+        outputs = tf.TensorArray(tf.float32, size=self.seq_length)
         
         # Initial state and previous output
-        state = tf.zeros((batch_size, self.rnn_size), dtype=tf.float32)
         prev_output = tf.zeros([batch_size, self.num_char_classes])
+        state = [tf.zeros([batch_size, self.rnn_size])] * 2
+        
 
-        # for i in range(timestep):
-        for i in tf.range(1):
-            print(prev_output.shape)
-            output, state = self.cell(prev_output, state)
-            print(output.shape)
-            outputs = outputs.append(output)
+        for t in tf.range(self.seq_length):
+            # Concat previous output and the image feature to create the input
+            image_feature = self.get_image_feature(f_pool, t)
+            x_t = tf.concat([image_feature, prev_output], axis=1)
+            
+            # Run through the cell
+            output, state = self.cell(x_t, state)
+
+            # Softmax for the logits then OH encode
+            output = self.char_logit(output)
+            output = self.char_one_hot(output)
+            outputs.write(t, output)
+
+            # If training, ground truth will be passed and should be used
+            # for autoregression, for inference use actual network out
             if ground_truth is not None:
-                prev_output = ground_truth[:, i, :]
+                prev_output = ground_truth[:, t, :]
             else:
-                prev_output = self.char_logit(output, i)
-                prev_output = self.char_one_hot(prev_output)
-
-
-        return tf.transpose(outputs.stack(), [1, 0, 2]), state
+                prev_output = output
+            
+        sequence_output = tf.transpose(outputs.stack(), [1, 0, 2])
+        print(sequence_output.shape)
+        return sequence_output, state
 
     @tf.function
-    def get_image_feature(self, f_enc, char_index):
+    def get_image_feature(self, f_pool, char_index):
         """Returns a subset of image features for a character.
         Args:
         char_index: an index of a character.
@@ -59,18 +69,18 @@ class ChaRNN(layers.Layer):
         A tensor with shape [batch_size, ?]. The output depth depends on the
         depth of input net.
         """
-        batch_size, features_num, _ = f_enc.shape
+        
+        batch_size, features_num, _ = f_pool.shape
         slice_len = int(features_num / self.seq_length)
         # In case when features_num != seq_length, we just pick a subset of image
         # features, this choice is arbitrary and there is no intuitive geometrical
         # interpretation. If features_num is not dividable by seq_length there will
-        # be unused image features.
-        f_slice = f_enc[:, char_index:char_index + slice_len, :]
+        f_slice = tf.slice(f_pool, [0, char_index, 0], [-1, slice_len, -1])
         feature = tf.reshape(f_slice, [batch_size, -1])
         return feature
 
     @tf.function
-    def char_logit(self, inputs, char_index):
+    def char_logit(self, inputs):
         """Creates logits for a character if required.
         Args:
         inputs: A tensor with shape [batch_size, ?] (depth is implementation
@@ -79,9 +89,9 @@ class ChaRNN(layers.Layer):
         Returns:
         A tensor with shape [batch_size, num_char_classes]
         """
-        if char_index not in self._char_logits:
-            self._char_logits[char_index] = inputs * self._softmax_w + self._softmax_b
-        return self._char_logits[char_index]
+        # if char_index not in self._char_logits:
+        #     self._char_logits[char_index] = inputs * self._softmax_w + self._softmax_b
+        return tf.matmul(inputs, self._softmax_w) + self._softmax_b
 
     @tf.function
     def char_one_hot(self, logit):
@@ -92,6 +102,6 @@ class ChaRNN(layers.Layer):
         A tensor with shape [batch_size, num_char_classes]
         """
         prediction = tf.argmax(logit, axis=1)
-        return slim.one_hot_encoding(prediction, self._params.num_char_classes)
+        return tf.one_hot(prediction, depth=self.num_char_classes)
 
 
