Added data_source module for loading TFRecords. Also: - Implemented variable number of views, as in paper. - Removed sparse/dense option on loss softmax - now always sparse (one hot) - Fixed error where rnn softmax weights werent being tracked - Fixed error in accuracy calculation in trainer - Added FSNS charset file

src/charset_mapper.py
@@ -35,7 +35,8 @@ class CharsetMapper(object):
                 text: a tensor with shape [batch_size,]
             """
         char_seq = tf.strings.bytes_split(text)
-        char_seq = tf.concat([char_seq, [['?'] * self.max_sequence_length]], axis=0)
+        char_seq = tf.concat(
+            [char_seq, [['?'] * self.max_sequence_length]], axis=0)
         dense_seq = tf.sparse.to_dense(char_seq.to_sparse(), default_value='?')
         dense_seq = dense_seq[:-1, :]
         return self.table_text.lookup(dense_seq)

src/main.py
@@ -1,17 +1,22 @@
 from trainer import Trainer
+from data_source import DataSource
+from model import OCRModel
 import tensorflow as tf
 import utils
 
-tf.random.set_seed(1)
+tf.random.set_seed(54321)
 
-trainer = Trainer()
+rnn_size = 256
+batch_size = 16
 
-train_set = tf.data.Dataset.list_files(str('data/simple_images/train/*'))
-train_set = train_set.map(utils.process_path, num_parallel_calls=5)
+data_source = DataSource()
+model = OCRModel(input_shape=data_source.config['image_shape'],
+                 seq_length=data_source.config['max_sequence_length'],
+                 rnn_size=rnn_size,
+                 charset=data_source.charset,
+                 num_views=data_source.config['num_of_views'])
+trainer = Trainer(model, data_source.config['null_code'])
 
-# for image, label in train_set.take(1):
-#   print("Image shape: ", image.numpy().shape)
-#   print("Label: ", label.numpy())
-
-train_set = train_set.batch(32)
-trainer.train(25, train_set)
+train_set = data_source.sets['train'].batch(batch_size)
+num_batches = int(data_source.config['splits']['train']['size'] / batch_size)
+trainer.train(25, train_set, num_batches)

src/model.py
@@ -4,20 +4,20 @@ import tensorflow as tf
 import tensorflow_addons as tfa
 import utils
 from model_parameters import default_mparams, OutputEndpoints
-from tensorflow.keras.layers import Dense, Flatten, Conv2D
 from tensorflow.keras import Model
 from tensorflow.keras.applications.inception_resnet_v2 import InceptionResNetV2, preprocess_input
 from charset_mapper import CharsetMapper
 from rnn import ChaRNN
 
 
-class ReaderModel(tf.keras.Model):
-    def __init__(self, input_shape, seq_length, rnn_size, charset):
-        super(ReaderModel, self).__init__()
+class OCRModel(tf.keras.Model):
+    def __init__(self, input_shape, seq_length, rnn_size, charset, num_views):
+        super().__init__()
         self._mparams = default_mparams()
         self.num_char_classes = len(charset)
         self.seq_length = seq_length
         self.charset = charset
+        self.num_views = num_views
 
         self.feature_enc = InceptionResNetV2(
             weights='imagenet', input_shape=input_shape, include_top=False)
@@ -25,6 +25,7 @@ class ReaderModel(tf.keras.Model):
             self.feature_enc.get_layer('mixed_6a').output]
         self.feature_enc.trainable = False
         self.rnn = ChaRNN(rnn_size, self.seq_length, self.num_char_classes)
+
         self.character_mapper = CharsetMapper(self.charset, self.seq_length)
 
     def call(self, x):
@@ -33,11 +34,12 @@ class ReaderModel(tf.keras.Model):
         input_image, ground_truth = x
 
         # Encode the image using resnet
-        f = self.feature_enc(input_image)
+        f_views = [self.feature_enc(x) for x in tf.split(
+            input_image, self.num_views, axis=2)]
 
         # Add the spacial coords
-        f_enc = self.encode_coords(f)
-        f_pool = self.pool_views(f_enc)
+        f_views_enc = [self.encode_coords(f_view) for f_view in f_views]
+        f_pool = self.pool_views(f_views_enc)
 
         # Generate the logits from the sequential model
         if ground_truth is not None:
@@ -55,35 +57,37 @@ class ReaderModel(tf.keras.Model):
             predicted_scores=predicted_scores,
             predicted_text=predicted_text)
 
-    def encode_coords(self, f):
+    def encode_coords(self, f_view):
         """Adds one-hot encoding of coordinates to different views in the networks.
         For each "pixel" of a feature map it adds a onehot encoded x and y
         coordinates.
         Args:
-        net: a tensor of shape=[batch_size, height, width, num_features]
+        f_view: a tensor of shape=[batch_size, height, width, num_features]
         Returns:
         a tensor with the same height and width, but altered feature_size.
         """
-        batch_size, h, w, _ = f.shape
+        batch_size, h, w, _ = f_view.shape
         x, y = tf.meshgrid(tf.range(w), tf.range(h))
         w_loc = tf.one_hot(x, depth=w)
         h_loc = tf.one_hot(y, depth=h)
         loc = tf.concat([h_loc, w_loc], 2)
-        loc = tf.tile(tf.expand_dims(loc, 0), [batch_size, 1, 1, 1])
-        return tf.concat([f, loc], 3)
+        loc = tf.tile(tf.expand_dims(loc, 0),
+                      tf.convert_to_tensor([batch_size, 1, 1, 1]))
+        return tf.concat([f_view, loc], 3)
 
-    def pool_views(self, f_enc):
+    def pool_views(self, f_views_enc):
         """Combines output of multiple convolutional towers into a single tensor.
         It stacks towers one on top another (in height dim) in a 4x1 grid.
         The order is arbitrary design choice and shouldn't matter much.
         Args:
-        nets: list of tensors of shape=[batch_size, height, width, num_features].
+        f_views_enc: list of tensors of shape=[batch_size, height, width, num_features].
         Returns:
         A tensor of shape [batch_size, seq_length, features_size].
         """
-        batch_size = f_enc.shape[0]
-        feature_size = f_enc.shape[3]
-        return tf.reshape(f_enc, [batch_size, -1, feature_size])
+        merged = tf.concat(f_views_enc, 1)
+        batch_size = merged.shape[0]
+        feature_size = merged.shape[3]
+        return tf.reshape(merged, [batch_size, -1, feature_size])
 
     def char_predictions(self, chars_logit):
         """Returns confidence scores (softmax values) for predicted characters.
@@ -112,8 +116,8 @@ class ReaderModel(tf.keras.Model):
     def loss(self, labels, chars_logits):
         """Creates all losses required to train the model.
         Args:
-        data: InputEndpoints namedtuple.
-        endpoints: Model namedtuple.
+        labels: labels as strings
+        chars_logits: the output of the model as logits
         Returns:
         Total loss.
         """
@@ -146,7 +150,7 @@ class ReaderModel(tf.keras.Model):
                 dtype=tf.int64)
             known_char = tf.not_equal(chars_labels, reject_char)
             weights = tf.cast(known_char, dtype=tf.float32)
-        
+
         if mparams.label_smoothing > 0:
             chars_labels = self.label_smoothing_regularization(
                 chars_labels, mparams.label_smoothing)
@@ -157,11 +161,6 @@ class ReaderModel(tf.keras.Model):
         crossent = tf.nn.softmax_cross_entropy_with_logits(
             logits=chars_logits, labels=chars_labels, axis=2)
         crossent *= weights
-        # crossent = tf.reduce_sum(
-        #     input_tensor=crossent, axis=1)
-        # total_size = tf.reduce_sum(
-        #     input_tensor=weights, axis=1)
-        # crossent = tf.math.divide_no_nan(crossent, total_size)
         return crossent
 
     def label_smoothing_regularization(self, chars_labels, weight=0.1):
@@ -179,24 +178,3 @@ class ReaderModel(tf.keras.Model):
         pos_weight = 1.0 - weight
         neg_weight = weight / self.num_char_classes
         return one_hot_labels * pos_weight + neg_weight
-
-    def get_softmax_loss_fn(self, label_smoothing):
-        """Returns sparse or dense loss function depending on the label_smoothing.
-            Args:
-            label_smoothing: weight for label smoothing
-            Returns:
-            a function which takes labels and predictions as arguments and returns
-            a softmax loss for the selected type of labels (sparse or dense).
-            """
-        if label_smoothing > 0:
-
-            def loss_fn(labels, logits):
-                return (tf.nn.softmax_cross_entropy_with_logits(
-                    logits=logits, labels=labels))
-        else:
-
-            def loss_fn(labels, logits):
-                return tf.nn.sparse_softmax_cross_entropy_with_logits(
-                    logits=logits, labels=labels)
-
-        return loss_fn

src/rnn.py
@@ -10,22 +10,18 @@ class ChaRNN(layers.Layer):
     '''
 
     def __init__(self, rnn_size, seq_length, num_char_classes):
-        super(ChaRNN, self).__init__()
+        super().__init__()
         self.rnn_size = rnn_size
         self.num_char_classes = num_char_classes
         self.seq_length = tf.constant(seq_length)
         self.cell = layers.LSTMCell(self.rnn_size)
         self.dense = layers.Dense(self.num_char_classes)
-        self._char_logits = {}
-
-        w_init = tf.random_normal_initializer()
-        self._softmax_w = tf.Variable(initial_value=w_init(shape=(self.rnn_size, self.num_char_classes),
-                                                           dtype='float32'),
-                                      trainable=True)
-        self._softmax_b = tf.Variable(initial_value=w_init(shape=(self.num_char_classes,),
-                                                           dtype='float32'),
-                                      trainable=True)
-
+        self._softmax_w = self.add_weight(shape=(self.rnn_size, self.num_char_classes),
+                                          initializer='random_normal',
+                                          trainable=True)
+        self._softmax_b = self.add_weight(shape=(self.num_char_classes,),
+                                          initializer='zeros',
+                                          trainable=True)
 
     def call(self, inputs):
         f_pool, ground_truth = inputs
@@ -47,17 +43,17 @@ class ChaRNN(layers.Layer):
             # Softmax for the logits then OH encode
             output = self.char_logit(output)
             outputs = outputs.write(t, output)
-            
+
             # If training, ground truth will be passed and should be used
             # for autoregression, for inference use actual network out
             if ground_truth is not None:
-                prev_output = tf.reshape(tf.one_hot(ground_truth[:, t], depth=self.num_char_classes), shape=[
+                char = ground_truth[:, t]
+                prev_output = tf.reshape(tf.one_hot(char, depth=self.num_char_classes), shape=[
                                          batch_size, self.num_char_classes])
             else:
                 output = self.char_one_hot(output)
                 prev_output = output
 
-        
         sequence_output = tf.transpose(outputs.stack(), [1, 0, 2])
         return sequence_output, state
 
@@ -75,7 +71,8 @@ class ChaRNN(layers.Layer):
         # In case when features_num != seq_length, we just pick a subset of image
         # features, this choice is arbitrary and there is no intuitive geometrical
         # interpretation. If features_num is not dividable by seq_length there will
-        f_slice = tf.slice(f_pool, [0, char_index, 0], [-1, slice_len, -1])
+        start = char_index * slice_len
+        f_slice = f_pool[:, start:start + slice_len, :]
         feature = tf.reshape(f_slice, [batch_size, -1])
         return feature
 
@@ -88,8 +85,6 @@ class ChaRNN(layers.Layer):
         Returns:
         A tensor with shape [batch_size, num_char_classes]
         """
-        # if char_index not in self._char_logits:
-        #     self._char_logits[char_index] = inputs * self._softmax_w + self._softmax_b
         return tf.matmul(inputs, self._softmax_w) + self._softmax_b
 
     def char_one_hot(self, logit):

src/trainer.py
@@ -3,35 +3,31 @@ import sys
 from timebudget import timebudget
 import tensorflow_addons as tfa
 import tensorflow as tf
-from model import ReaderModel
 from os.path import join
 import time
 from datetime import datetime
 
 
 class Trainer(object):
-    def __init__(self):
-        self.optimizer = tf.compat.v1.train.MomentumOptimizer(0.004, 0.9)
+    def __init__(self, model, null_code):
+        # self.optimizer = tf.compat.v1.train.MomentumOptimizer(0.004, 0.9)
+        self.optimizer = tf.keras.optimizers.Adadelta(learning_rate=0.001)
         self.loss_history = []
-        chars = '?abcdefghijklmnopqrstuvwxyz'
-        charset = dict(enumerate(chars))
-        self.model = ReaderModel(input_shape=(240, 240, 3),
-                                 seq_length=8,
-                                 rnn_size=256,
-                                 charset=charset)
+        self.model = model
+        self.null_code = null_code
         ts = time.time()
         self.writer = tf.summary.create_file_writer(
             join("logdir", datetime.fromtimestamp(ts).strftime('%Y%m%d%H%M%S')))
 
-    def _mask_predictions(self, predicted_chars, labels):
+    def _get_mask(self, labels):
         # Suppose that reject character is always 0.
         reject_char = tf.constant(
-            0,
-            shape=predicted_chars.shape,
+            self.null_code,
+            shape=labels.shape,
             dtype=tf.int64)
         known_char = tf.not_equal(labels, reject_char)
         mask = tf.cast(known_char, dtype=tf.int32)
-        return predicted_chars * mask
+        return mask
 
     def train_step(self, images, labels):
         with tf.GradientTape() as tape:
@@ -43,7 +39,7 @@ class Trainer(object):
             loss_value, label_enc = self.model.loss(
                 labels, output_endpoint.chars_logit)
 
-        # self.loss_history.append(loss_value.numpy().mean())
+        self.loss_history.append(loss_value.numpy().mean())
         grads = tape.gradient(loss_value, self.model.trainable_variables)
         self.optimizer.apply_gradients(
             zip(grads, self.model.trainable_variables))
@@ -51,14 +47,14 @@ class Trainer(object):
         return loss_value, label_enc, output_endpoint.predicted_chars
 
     @timebudget
-    def train(self, epochs, dataset):
+    def train(self, epochs, dataset, num_batches):
+        tf.summary.trace_on()
         print('Beginning training...')
         for epoch in range(epochs):
             epoch_loss_avg = tf.keras.metrics.Mean()
-            epoch_accuracy = tf.keras.metrics.CategoricalAccuracy()
+            epoch_accuracy = tf.keras.metrics.Accuracy()
 
-            progbar = tf.keras.utils.Progbar(
-                len(list(dataset.as_numpy_iterator())))
+            progbar = tf.keras.utils.Progbar(num_batches)
 
             # Training loop for each batch
             for (batch, (images, labels)) in enumerate(dataset):
@@ -68,14 +64,11 @@ class Trainer(object):
 
                 # Record the training loss and accuracy
                 epoch_loss_avg(loss_value)
-                masked_predictions = self._mask_predictions(
-                    predicted_chars, label_enc)
-                epoch_accuracy(label_enc, masked_predictions)
+                mask = self._get_mask(label_enc)
+                epoch_accuracy(label_enc, predicted_chars, sample_weight=mask)
 
                 # Update the progress bar
                 progbar.update(batch+1)
-
-            #
 
             # Write metrics
             with self.writer.as_default():
@@ -83,10 +76,13 @@ class Trainer(object):
                     'train_loss', epoch_loss_avg.result(), step=epoch)
                 tf.summary.scalar(
                     'train_accuracy', epoch_accuracy.result(), step=epoch)
+
             self.writer.flush()
 
             print("Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch,
                                                                         epoch_loss_avg.result(),
                                                                         epoch_accuracy.result()))
+        with self.writer.as_default():
+            tf.summary.trace_export("graph", step=0)
 
         print('Training complete.')

src/utils.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow.keras.applications.inception_resnet_v2 import preprocess_input
 import os
 
+
 def logits_to_log_prob(logits):
     """Computes log probabilities using numerically stable trick.
     This uses two numerical stability tricks:
@@ -18,30 +19,35 @@ def logits_to_log_prob(logits):
     """
 
     reduction_indices = len(logits.shape.as_list()) - 1
-    max_logits = tf.math.reduce_max(logits, axis=reduction_indices, keepdims=True)
+    max_logits = tf.math.reduce_max(
+        logits, axis=reduction_indices, keepdims=True)
     safe_logits = tf.subtract(logits, max_logits)
-    sum_exp = tf.math.reduce_sum(tf.exp(safe_logits), axis=reduction_indices,keepdims=True)
+    sum_exp = tf.math.reduce_sum(
+        tf.exp(safe_logits), axis=reduction_indices, keepdims=True)
     log_probs = tf.math.subtract(safe_logits, tf.math.log(sum_exp))
     return log_probs
 
+
 def get_label(file_path):
-  # convert the path to a list of path components
-  parts = tf.strings.split(file_path, os.path.sep)
-  label = tf.strings.split(parts[-1], '.')[0]
-  # The second to last is the class-directory
-  return label
+    # convert the path to a list of path components
+    parts = tf.strings.split(file_path, os.path.sep)
+    label = tf.strings.split(parts[-1], '.')[0]
+    # The second to last is the class-directory
+    return label
+
 
 def decode_img(img):
-  # convert the compressed string to a 3D uint8 tensor
-  img = tf.image.decode_jpeg(img, channels=3)
-  # Use `convert_image_dtype` to convert to floats in the [0,1] range.
-  img = tf.image.convert_image_dtype(img, tf.float32)
-  # resize the image to the desired size.
-  return preprocess_input(tf.image.resize(img, [240, 240]))
+    # convert the compressed string to a 3D uint8 tensor
+    img = tf.image.decode_jpeg(img, channels=3)
+    # Use `convert_image_dtype` to convert to floats in the [0,1] range.
+    img = tf.image.convert_image_dtype(img, tf.float32)
+    # resize the image to the desired size.
+    return preprocess_input(tf.image.resize(img, [240, 240]))
+
 
 def process_path(file_path):
-  label = get_label(file_path)
-  # load the raw data from the file as a string
-  img = tf.io.read_file(file_path)
-  img = decode_img(img)
-  return img, label
+    label = get_label(file_path)
+    # load the raw data from the file as a string
+    img = tf.io.read_file(file_path)
+    img = decode_img(img)
+    return img, label