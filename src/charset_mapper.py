118f72b929751098d23160f75c75560b0e5282c0
Conor Sexton <sexton.conor@gmail.com>
2025-06-09 14:14:35 -0700

    Formatting

--- src/charset_mapper.py
+++ src/charset_mapper.py
@@ -8,7 +8,7 @@ class CharsetMapper(object):
     Make sure you call tf.tables_initializer().run() as part of the init op.
     """
 
-    def __init__(self, charset, max_sequence_length, null_char=u'\u2591', null_id=133):
+    def __init__(self, charset, max_sequence_length, null_char="\u2591", null_id=133):
         """Creates a lookup table.
         Args:
             charset: a dictionary with id-to-character mapping.
@@ -19,26 +19,29 @@ class CharsetMapper(object):
         self.null_char = null_char
         self.null_id = null_id
         self.table_ids = tf.lookup.StaticHashTable(
-            tf.lookup.KeyValueTensorInitializer(ids_tensor, chars_tensor, key_dtype=tf.int64, value_dtype=tf.string), null_char)
+            tf.lookup.KeyValueTensorInitializer(ids_tensor, chars_tensor, key_dtype=tf.int64, value_dtype=tf.string),
+            null_char,
+        )
 
         self.table_text = tf.lookup.StaticHashTable(
-            tf.lookup.KeyValueTensorInitializer(chars_tensor, ids_tensor, key_dtype=tf.string, value_dtype=tf.int64), null_id)
+            tf.lookup.KeyValueTensorInitializer(chars_tensor, ids_tensor, key_dtype=tf.string, value_dtype=tf.int64),
+            null_id,
+        )
 
     def get_text(self, ids):
         """Returns a string corresponding to a sequence of character ids.
-            Args:
-                ids: a tensor with shape [batch_size, max_sequence_length]
-            """
+        Args:
+            ids: a tensor with shape [batch_size, max_sequence_length]
+        """
         return tf.strings.reduce_join(self.table_ids.lookup(tf.cast(ids, dtype=tf.int64)), axis=1)
 
     def get_ids(self, text):
         """Returns a sequence of character ids corresponding to a string.
-            Args:
-                text: a tensor with shape [batch_size,]
-            """
+        Args:
+            text: a tensor with shape [batch_size,]
+        """
         char_seq = tf.strings.bytes_split(text)
-        char_seq = tf.concat(
-            [char_seq, [[self.null_char] * self.max_sequence_length]], axis=0)
+        char_seq = tf.concat([char_seq, [[self.null_char] * self.max_sequence_length]], axis=0)
         dense_seq = tf.sparse.to_dense(char_seq.to_sparse(), default_value=self.null_char)
         dense_seq = dense_seq[:-1, :]
         return self.table_text.lookup(dense_seq)
